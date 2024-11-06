a496aaeecf23c8ac6a75de7b49e2b8cc378c9f00
kimjeonggeon <kjkkmk4329@bizbee.co.kr>
2024-11-06 14:55:02 +0900

    예외처리 - 체크예외

--- src/exception/basic/checked/Client.java
+++ src/exception/basic/checked/Client.java
@@ -2,9 +2,11 @@ package exception.basic.checked;
 
 public class Client {
     public void call() throws  MyCheckedException {
-
+        //throws는 해결 못해서 밖으로 던질거야
         /*문제상황*/
-// throw와  throws의 차이는 throws는 문제를 터뜨리는것이고 throws는 해결 못해하고 던지는 것         
+        // throw는 새로운 예외를 만드는 것 예외도 객체이기 때문에 new로 생성하고 예외 발생
+        // 예외를 던지면 해결을 못해서 throws로 밖으로 던진다.
+        //이 아래로 실행안되고 밖으로 던진다.
         throw new MyCheckedException("ex");
 
     }
