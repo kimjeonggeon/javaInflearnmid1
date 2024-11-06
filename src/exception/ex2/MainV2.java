05bb329ce98c20210d4c613c2f85718290ca68a7
kimjeonggeon <kjkkm42397@naver.com>
2024-11-06 16:29:51 +0900

    예외처리 도입시작

--- src/exception/ex2/MainV2.java
+++ src/exception/ex2/MainV2.java
@@ -1,12 +1,12 @@
-package exception.ex01;
+package exception.ex2;
 
 import exception.NetworkServiceV0;
 
 import java.util.Scanner;
 
-public class MainV12 {
-    public static void main(String[ ] args){
-        NetworkServiceV1_3 service = new NetworkServiceV1_3();
+public class MainV2 {
+    public static void main(String[ ] args) throws NetworkClientExceptionV2 {
+        NetworkServiceV2_1 service = new NetworkServiceV2_1();
         Scanner scanner = new Scanner(System.in);
         while(true){
             System.out.println("전송 문자");
@@ -18,5 +18,7 @@ public class MainV12 {
             System.out.println();
         }
         System.out.println("프로그램을 종료합니다.");
+        //checkpoint 현재 코드에선 disconnect가 안됐고 복구가 안된다.
+
     }
 }
---
+++ src/exception/ex2/NetworkClientV2.java
@@ -0,0 +1,38 @@
+package exception.ex2;
+
+public class NetworkClientV2 {
+
+    private final String address;
+    public boolean connectError;
+    public boolean sendError;
+
+    public NetworkClientV2(String address) {
+        this.address = address;
+    }
+
+    public String connect() throws NetworkClientExceptionV2{
+        if(connectError){
+            throw new NetworkClientExceptionV2("connectError",address+"서버연결실패");
+        }
+        System.out.println("서버 연결 성공" + address);
+        return "success";
+    }
+
+    public String send(String data) throws NetworkClientExceptionV2{
+        if(sendError){
+            throw new NetworkClientExceptionV2("sendError",address+"서버 데이터 전송 실패");
+        }
+        return "success" + data;
+    }
+    public void disconnect(){
+        System.out.println("서버연결 해제");
+    }
+    public void initError(String data){
+        if(data.contains("error")){
+            connectError = true;
+        }
+        if(data.contains("error2")){
+            sendError = true;
+        }
+    }
+}
